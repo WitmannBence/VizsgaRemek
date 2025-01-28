18e0b9793fe7a104f20589a76f06c3778823989f
budahazim@kkszki.hu <budahazim@kkszki.hu>
2025-01-28 14:08:32 +0100

    GetAllusers

---
+++ vizsgaremek/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using vizsgaremek.Models;
+
+namespace vizsgaremek.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers() 
+        {
+            using (var context = new VizsgaremekContext())
+            {
+                try
+                {
+                    List<User> userList = new List<User>();
+                    {
+                        userList = context.Users.ToList();
+
+                    }
+                    return Ok(userList);
+                }
+                catch (Exception ex)
+                {
+
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    }
+}
