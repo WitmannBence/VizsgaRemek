4ffe7d4731be2972d90ca7bea08237fcdec0ea39
WitmannBence <witmannb@kkszki.hu>
2025-01-28 14:47:48 +0100

    Base of ServicePage added

--- ServicesPage.js
+++ ServicesPage.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
 export default function ServicesPage() {
-  return (
-    
-  )
-}
+
+    function Get() {
+
+        fetch('https://localhost:7025/api/User')
+        .then(response => response.json())
+        .then(data => {setdata(data);console.log(data);})
+        
+      }
+
+      return (
+        <div>
+        {data.map(function(player){
+          return(
+          <Card
+          get = {Get}
+          />
+        
+          )
+        }
+      )
+      
+    }
+    </div>
+    )}
