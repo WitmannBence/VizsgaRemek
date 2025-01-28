793359d3a9eb72ca2bcf89b480ad641d082a5cd1
Admin <Admin@29-11>
2025-01-28 12:01:46 +0100

    Add project files.

---
+++ vizsgaremek/Models/Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vizsgaremek.Models;
+
+public partial class Service
+{
+    public int ServiceId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string ServiceName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public string? Category { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual ICollection<Request> Requests { get; set; } = new List<Request>();
+
+    public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ vizsgaremek/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vizsgaremek.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int PrivilegeId { get; set; }
+
+    public decimal? TimeBalance { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual ICollection<Request> Requests { get; set; } = new List<Request>();
+
+    public virtual ICollection<Service> Services { get; set; } = new List<Service>();
+
+    public virtual ICollection<Transaction> TransactionReceivers { get; set; } = new List<Transaction>();
+
+    public virtual ICollection<Transaction> TransactionSenders { get; set; } = new List<Transaction>();
+}
