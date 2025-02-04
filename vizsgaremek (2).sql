1de7510f43ad9ee187339faf15159322bad35598
budahazim <90623826+budahazim@users.noreply.github.com>
2025-02-04 13:09:24 +0100

    Add files via upload
    hash user login

---
+++ vizsgaremek (2).sql
@@ -0,0 +1,240 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Feb 04. 13:08
+-- Kiszolgáló verziója: 10.4.20-MariaDB
+-- PHP verzió: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `vizsgaremek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `categories`
+--
+
+CREATE TABLE `categories` (
+  `CategoryID` int(11) NOT NULL,
+  `CategoryName` varchar(50) NOT NULL,
+  `Description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `privileges`
+--
+
+CREATE TABLE `privileges` (
+  `id` int(1) NOT NULL,
+  `nev` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `szint` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `privileges`
+--
+
+INSERT INTO `privileges` (`id`, `nev`, `description`, `szint`) VALUES
+(1, 'Felhasználó', 'Alap jogosultságokkal rendelkezik', 1),
+(2, 'Admin', 'Teljes hozzáférés', 9);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `requests`
+--
+
+CREATE TABLE `requests` (
+  `RequestID` int(11) NOT NULL,
+  `RequesterID` int(11) NOT NULL,
+  `ServiceID` int(11) NOT NULL,
+  `Status` enum('Pending','Approved','Completed','Rejected') DEFAULT 'Pending',
+  `RequestedAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `services`
+--
+
+CREATE TABLE `services` (
+  `ServiceID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `ServiceName` varchar(100) NOT NULL,
+  `Description` text DEFAULT NULL,
+  `Category` varchar(50) DEFAULT NULL,
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `TransactionID` int(11) NOT NULL,
+  `SenderID` int(11) NOT NULL,
+  `ReceiverID` int(11) NOT NULL,
+  `ServiceID` int(11) NOT NULL,
+  `TimeAmount` decimal(10,2) NOT NULL,
+  `Description` text DEFAULT NULL,
+  `TransactionDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL,
+  `FelhasznaloNev` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `TeljesNev` varchar(60) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `SALT` varchar(64) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `HASH` varchar(64) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `Email` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `Jogosultsag` int(1) NOT NULL,
+  `Aktiv` int(1) NOT NULL,
+  `RegisztracioDatuma` datetime DEFAULT current_timestamp(),
+  `ProfilKepUtvonal` varchar(64) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `TimeBalance` decimal(10,2) DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `user`
+--
+
+INSERT INTO `user` (`UserID`, `FelhasznaloNev`, `TeljesNev`, `SALT`, `HASH`, `Email`, `Jogosultsag`, `Aktiv`, `RegisztracioDatuma`, `ProfilKepUtvonal`, `TimeBalance`) VALUES
+(1, 'LakatosI', 'Lakatos István', 'jQGX8grO1yjNqhiZbtROcseiqj1NVZJd2iqlfxPx1GKLJ9H8smnLJ9dloScCK6Zp', 'dcedbd2d352d19c6eae0dfb12271b74d985c825b8d774afd2abd0d101b6e57ef', 'lakatosi@gmail.com', 9, 1, '2024-11-25 07:33:49', '', '0.00');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`CategoryID`),
+  ADD UNIQUE KEY `CategoryName` (`CategoryName`);
+
+--
+-- A tábla indexei `privileges`
+--
+ALTER TABLE `privileges`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `szint` (`szint`);
+
+--
+-- A tábla indexei `requests`
+--
+ALTER TABLE `requests`
+  ADD PRIMARY KEY (`RequestID`),
+  ADD KEY `RequesterID` (`RequesterID`),
+  ADD KEY `ServiceID` (`ServiceID`);
+
+--
+-- A tábla indexei `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`ServiceID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- A tábla indexei `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`TransactionID`),
+  ADD KEY `SenderID` (`SenderID`),
+  ADD KEY `ReceiverID` (`ReceiverID`),
+  ADD KEY `ServiceID` (`ServiceID`);
+
+--
+-- A tábla indexei `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `FelhasznaloNev` (`FelhasznaloNev`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `Jogosultsag` (`Jogosultsag`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `privileges`
+--
+ALTER TABLE `privileges`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT a táblához `requests`
+--
+ALTER TABLE `requests`
+  MODIFY `RequestID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `services`
+--
+ALTER TABLE `services`
+  MODIFY `ServiceID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `TransactionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `requests`
+--
+ALTER TABLE `requests`
+  ADD CONSTRAINT `requests_ibfk_2` FOREIGN KEY (`ServiceID`) REFERENCES `services` (`ServiceID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `requests_ibfk_3` FOREIGN KEY (`RequesterID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Megkötések a táblához `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_3` FOREIGN KEY (`ServiceID`) REFERENCES `services` (`ServiceID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
