a4944bfdb7c71aca74571edc9b39212a0e8f1463
budahazim <90623826+budahazim@users.noreply.github.com>
2025-02-10 12:25:26 +0100

    Add files via upload

--- vizsgaremek.sql
+++ vizsgaremek.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Feb 07. 12:04
--- Kiszolgáló verziója: 10.4.20-MariaDB
--- PHP verzió: 7.3.29
+-- Létrehozás ideje: 2025. Feb 10. 12:23
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `categories` (
   `CategoryID` int(11) NOT NULL,
   `CategoryName` varchar(50) NOT NULL,
   `Description` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -44,7 +44,7 @@ CREATE TABLE `privileges` (
   `nev` varchar(100) NOT NULL,
   `description` text NOT NULL,
   `szint` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- A tábla adatainak kiíratása `privileges`
@@ -66,7 +66,7 @@ CREATE TABLE `requests` (
   `ServiceID` int(11) NOT NULL,
   `Status` enum('Pending','Approved','Completed','Rejected') DEFAULT 'Pending',
   `RequestedAt` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -82,7 +82,17 @@ CREATE TABLE `services` (
   `Description` text DEFAULT NULL,
   `Category` varchar(50) DEFAULT NULL,
   `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `services`
+--
+
+INSERT INTO `services` (`ServiceID`, `UserID`, `ServiceName`, `TimeCost`, `Description`, `Category`, `CreatedAt`) VALUES
+(1, 0, 'cccc', 10.00, 'aaaa', 'bbbb', '2025-02-10 09:09:16'),
+(2, 0, 'macsek', 10.00, 'levi', 'gyenge', '2025-02-10 09:43:22'),
+(3, 0, 'mascsek', 10.00, 'nagyon', 'gyenge', '2025-02-10 09:45:00'),
+(4, 1, 'macsek', 10.00, 'legnagyobb', 'kutya', '2025-02-10 09:49:14');
 
 -- --------------------------------------------------------
 
@@ -98,7 +108,7 @@ CREATE TABLE `transactions` (
   `TimeAmount` decimal(10,2) NOT NULL,
   `Description` text DEFAULT NULL,
   `TransactionDate` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -108,15 +118,15 @@ CREATE TABLE `transactions` (
 
 CREATE TABLE `user` (
   `UserID` int(11) NOT NULL,
-  `FelhasznaloNev` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `TeljesNev` varchar(60) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `SALT` varchar(64) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `HASH` varchar(64) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `Email` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `FelhasznaloNev` varchar(100) NOT NULL,
+  `TeljesNev` varchar(60) NOT NULL,
+  `SALT` varchar(64) NOT NULL,
+  `HASH` varchar(64) NOT NULL,
+  `Email` varchar(100) NOT NULL,
   `Jogosultsag` int(1) NOT NULL,
   `Aktiv` int(1) NOT NULL,
   `RegisztracioDatuma` datetime DEFAULT current_timestamp(),
-  `ProfilKepUtvonal` varchar(64) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `ProfilKepUtvonal` varchar(64) NOT NULL,
   `TimeBalance` decimal(10,2) DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
@@ -125,7 +135,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`UserID`, `FelhasznaloNev`, `TeljesNev`, `SALT`, `HASH`, `Email`, `Jogosultsag`, `Aktiv`, `RegisztracioDatuma`, `ProfilKepUtvonal`, `TimeBalance`) VALUES
-(1, 'LakatosI', 'Lakatos István', 'zRNnKIdBWprmDl0y7opRWQdvSIDDaCknTrqli8zd0VQ3ilTziKHlAJcUmwR66laF', 'e01b3f5e704025326ed773a974c28694af262f98787441aaef327fc211b949da', 'lakatosi@gmail.com', 9, 1, '2024-11-25 07:33:49', '', '0.00');
+(1, 'LakatosI', 'Lakatos István', 'zRNnKIdBWprmDl0y7opRWQdvSIDDaCknTrqli8zd0VQ3ilTziKHlAJcUmwR66laF', 'e01b3f5e704025326ed773a974c28694af262f98787441aaef327fc211b949da', 'lakatosi@gmail.com', 9, 1, '2024-11-25 07:33:49', '', 0.00);
 
 -- --------------------------------------------------------
 
@@ -137,7 +147,7 @@ CREATE TABLE `user_services` (
   `id` int(11) NOT NULL,
   `UserID` int(11) NOT NULL,
   `ServiceID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexek a kiírt táblákhoz
@@ -225,7 +235,7 @@ ALTER TABLE `requests`
 -- AUTO_INCREMENT a táblához `services`
 --
 ALTER TABLE `services`
-  MODIFY `ServiceID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ServiceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT a táblához `transactions`
@@ -272,7 +282,7 @@ ALTER TABLE `user`
 -- Megkötések a táblához `user_services`
 --
 ALTER TABLE `user_services`
-  ADD CONSTRAINT `user_services_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_services_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `user_services_ibfk_2` FOREIGN KEY (`ServiceID`) REFERENCES `services` (`ServiceID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `user_services_ibfk_3` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
