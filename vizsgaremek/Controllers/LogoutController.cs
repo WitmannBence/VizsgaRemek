c645dac43eb1a64812ed4b01c362f3a0c087b4ba
budahazim@kkszki.hu <budahazim@kkszki.hu>
2025-02-14 11:20:25 +0100

    Logout(WIP)

---
+++ vizsgaremek/Controllers/LogoutController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vizsgaremek.DTOs;
+using vizsgaremek.Models;
+
+namespace vizsgaremek.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogoutController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logout(string uID)
+        {
+            using (var context = new VizsgaremekContext())
+            {
+                try
+                {
+                    if (Program.LoggedInUsers.ContainsKey(uID))
+                    {
+                        Program.LoggedInUsers.Remove(uID);
+                        return Ok("Sikeres kijelentkezés");
+                    }
+
+                    else if (!Program.LoggedInUsers.ContainsKey(uID))
+                    {
+                        return Unauthorized("Nem vagy bejelentkezve");
+                    }
+
+                    else
+                    {
+                        return BadRequest("Hiba történt");
+                    }
+                }
+
+
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+
+    }
+}
